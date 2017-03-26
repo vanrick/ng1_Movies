b6c1c39c6e3e1d2d3077bad774d01499de7decd9
Rick Van <x.rckvan@gmail.com>
2017-03-26 17:20:15 -0600

    finished angular movie app

--- 0 finished folder/ng1_movies/controllers/search.js
+++ 0 finished folder/ng1_movies/controllers/search.js
@@ -1,6 +1,25 @@
 app.controller('SearchController', function($scope, $http, $route, $routeParams, SearchFactory) {
   $scope.view = {}
-  $scope.view.searchy = SearchFactory.search
+  $scope.searchInput
+  $scope.view.data1
+  $scope.view.searchy = function(x){
+    SearchFactory.search1(x).then(function(data){
+      // console.log('SearchController ',data.data.Search);
+      $scope.view.data1 = [];
+      var arr = data.data.Search;
+      arr.forEach(function(entry){
 
-  console.log($scope.view.searchy);
+        if (entry.Poster == 'N/A') {
+          entry.Poster = 'http://www.freeiconspng.com/uploads/popcorn-png-19.png'
+        }
+        $scope.view.data1.push(entry)
+        // console.log('na ', entry.Poster);
+      })
+      // $scope.view.data1.push(arr)
+      // console.log($scope.view.data1);
+    })
+    $scope.searchInput = ''
+  }
+// http://www.freeiconspng.com/uploads/popcorn-png-19.png
+
 })
--- 0 finished folder/ng1_movies/factory/factory.js
+++ 0 finished folder/ng1_movies/factory/factory.js
@@ -1,18 +1,25 @@
 app.factory('SearchFactory', function ($routeParams, $http) {
+
   return {
-    search: function(){
-		$scope.view.data1 = [];
-		$scope.view.data2 = [];
-		$http.get(`http://www.omdbapi.com/?s=${$scope.searchInput}`).then(function(data){
-			var arr = data.data.Search;
-			for (var i = 0; i < arr.length; i++) {
-				if(i % 2 === 0){
-					$scope.view.data1.push(arr[i])
-				} else {
-					$scope.view.data2.push(arr[i])
-				}
-			}
+    // dataz1: function(){
+    //   return data1
+    // },
+    search1: function(x){
+      // var data1 = []
+      // var data2 = []
+		return $http.get(`http://www.omdbapi.com/?s=${x}`).then(function(data){
+			// var arr = data.data.Search;
+			// for (var i = 0; i < arr.length; i++) {
+			// 	if(i % 2 === 0){
+			// 		data1.push(arr[i])
+			// 	} else {
+			// 		data2.push(arr[i])
+			// 	}
+			// }
+      return data
 		})
+    // console.log('fact data1 ',data1);
+    // console.log('fact data2 ',data2);
 	}
   }
 });
