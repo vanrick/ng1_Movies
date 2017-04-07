0a6ef57da8ecf0fbc3ff06ad2733f8fe5a2443b2
Rick Van <x.rckvan@gmail.com>
2017-04-06 23:14:56 -0600

    deleted notes

--- 0 finished folder/ng1_movies/factory/factory.js
+++ 0 finished folder/ng1_movies/factory/factory.js
@@ -1,25 +1,14 @@
 app.factory('SearchFactory', function ($routeParams, $http) {
 
   return {
-    // dataz1: function(){
-    //   return data1
-    // },
+
     search1: function(x){
-      // var data1 = []
-      // var data2 = []
+
 		return $http.get(`http://www.omdbapi.com/?s=${x}`).then(function(data){
-			// var arr = data.data.Search;
-			// for (var i = 0; i < arr.length; i++) {
-			// 	if(i % 2 === 0){
-			// 		data1.push(arr[i])
-			// 	} else {
-			// 		data2.push(arr[i])
-			// 	}
-			// }
+
       return data
 		})
-    // console.log('fact data1 ',data1);
-    // console.log('fact data2 ',data2);
+
 	}
   }
 });
